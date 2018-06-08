0c7837a78dc3ad30a8c0a381689fe984e4e0e825
Ye980226 <18955993726@163.com>
2018-06-08 20:00:54 +0800

    api_fxdayu

--- dybitex/api/WSS/__init__.py
+++ dybitex/api/WSS/__init__.py
@@ -1,1 +1,1 @@
-from 数字货币.dybitex.api.WSS.api_vnpy.vnokex import OkexSpotApi, SPOT_CURRENCY, SPOT_SYMBOL, OKEX_SPOT_HOST
+from dybitex.api.WSS.api_vnpy.vnokex import OkexSpotApi, SPOT_CURRENCY, SPOT_SYMBOL, OKEX_SPOT_HOST
