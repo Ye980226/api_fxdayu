9535188cb3097c962da2edd2411046cae1959f7f
Ye980226 <18955993726@163.com>
2018-06-08 21:18:50 +0800

    api_fxdayu

--- dybitex/api/WSS/okcoin.py
+++ dybitex/api/WSS/okcoin.py
@@ -18,7 +18,6 @@ from threading import Thread
 from time import sleep
 
 from .api_vnpy.vnokex import OkexSpotApi, OKEX_SPOT_HOST
-from .api_vnpy.vtFunction import getJsonPath
 from vtObject import *
 EXCHANGE_OKEX = 'OKEX'		 # OKEX比特币交易所
 STATUS_REJECTED = '拒单'
@@ -48,6 +47,26 @@ statusMap[1] = STATUS_PARTTRADED
 statusMap[2] = STATUS_ALLTRADED
 statusMap[4] = STATUS_UNKNOWN
 ########################################################################
+# JSON配置文件路径
+jsonPathDict = {}
+def getJsonPath(name, moduleFile):
+    """
+    获取JSON配置文件的路径：
+    1. 优先从当前工作目录查找JSON文件
+    2. 若无法找到则前往模块所在目录查找
+    """
+    currentFolder = os.getcwd()
+    currentJsonPath = os.path.join(currentFolder, name)
+    if os.path.isfile(currentJsonPath):
+        jsonPathDict[name] = currentJsonPath
+        return currentJsonPath
+
+    moduleFolder = os.path.abspath(os.path.dirname(moduleFile))
+    moduleJsonPath = os.path.join(moduleFolder, '.', name)
+    jsonPathDict[name] = moduleJsonPath
+    return moduleJsonPath
+
+
 class OkexGateway:
     """OKEX交易接口"""
 
